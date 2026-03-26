c9247ed3037a62ac382efc993472c6afd929e1d9
linyihdfj <1370267164@qq.com>
2026-03-26 22:24:53 +0800

    fix: use BASE_URL for redirects and links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   // GitHub Pages configuration
-  site: "https://linyihdfj.github.io/Personal-Page/",
-  base: process.env.NODE_ENV === "production" ? "/bizarre-belt/" : "/",
+  site: "https://linyihdfj.github.io",
+  base: process.env.NODE_ENV === "production" ? "/Personal-Page/" : "/",
   output: "static",
 });
